fabd972ce678dac27afcf6b6cf1b09353a45f1bf
= <anna.sem@gmail.com>
2021-12-01 19:32:47 +0300

    Understandable assertion added

--- test_main_page.py
+++ test_main_page.py
@@ -7,3 +7,9 @@ def test_guest_can_go_to_login(browser, language):
     browser.implicitly_wait(10)
     browser.get(link)
     login_link=browser.find_elements_by_css_selector("#login_link")
+
+def test_guest_schould_see_login_link(browser):
+    link="http://selenium1py.pythonanywhere.com/"
+    page=MainPage(browser,link)
+    page.open()
+    page.should_be_login_link()
